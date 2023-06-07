c8959c57e661d8861fb6c8cc76aedaa35bdea1ac
Kizitora3000 <kutaku2000@gmail.com>
2023-06-07 10:46:13 +0900

    add: make cihpertext prediction dataset

--- make_dataset.py
+++ make_dataset.py
@@ -1,10 +1,52 @@
+import os
+import numpy as np
 import ctypes
 
-lib = ctypes.CDLL('./PRESENT_16.so')
+PRESENT_16 = ctypes.CDLL('./PRESENT_16.so')
 
-lib.PRESENT_16_wrapper_for_python.argtypes = [ctypes.c_int]
-lib.PRESENT_16_wrapper_for_python.restype = ctypes.c_int
+PRESENT_16.EncryptWrapper.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+PRESENT_16.EncryptWrapper.restype = ctypes.c_int
+PRESENT_16.DecryptWrapper.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+PRESENT_16.DecryptWrapper.restype = ctypes.c_int
 
-result = lib.PRESENT_16_wrapper_for_python(0x00)
+""" 
+wrapper example
 
-print(hex(result))
+ciphertext  = PRESENT_16.EncryptWrapper(secret_key, plaintext)
+decrypttext = PRESENT_16.DecryptWrapper(secret_key, ciphertext)
+"""
+
+"""
+notation
+
+CP: Ciphertext Prediction
+PR: Plaintext Recovery
+"""
+
+
+secret_key = (ctypes.c_int * 5)(0x0000, 0x0000, 0x0000, 0x0000, 0x0000)
+
+x_CP_dataset = []
+y_CP_dataset = []
+
+for plaintext in range(2**8):
+    ciphertext = PRESENT_16.EncryptWrapper(secret_key, plaintext)
+
+    x_CP_dataset.append(plaintext)
+    y_CP_dataset.append(ciphertext)
+
+split_train_and_test = len(x_CP_dataset) // 2
+
+x_CP_train = x_CP_dataset[:split_train_and_test]
+y_CP_train = y_CP_dataset[:split_train_and_test]
+
+x_CP_test = x_CP_dataset[split_train_and_test:]
+y_CP_test = y_CP_dataset[split_train_and_test:]
+
+if not os.path.exists("output"):
+    os.mkdir("output")
+
+np.save("output/x_CP_train", np.array(x_CP_train))
+np.save("output/y_CP_train", np.array(y_CP_train))
+np.save("output/x_CP_test",  np.array(x_CP_test))
+np.save("output/y_CP_test",  np.array(y_CP_test))
